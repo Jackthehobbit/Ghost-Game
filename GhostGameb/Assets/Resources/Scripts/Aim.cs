5fbebc9ea729400e4c76cb32b3555a320ba8298f
Jackthehobbit <scunnyrule45@hotmail.co.uk>
2014-03-29 03:16:51 +0000

    Comments.
    Added Comments to Aim and Flip Scripts

--- GhostGameb/Assets/Resources/Scripts/Aim.cs
+++ GhostGameb/Assets/Resources/Scripts/Aim.cs
@@ -1,30 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+/////////////////////////////////////////////
+// Script to control the mouse aim system////
+////////////////////////////////////////////
 public class Aim : MonoBehaviour {
-	PlayerMovement Player;
-	// Use this for initialization
+	PlayerMovement Player; // Var storing player object
+	Vector3 mouse_pos ;// Var to hold mouse position
+	Vector3 player_pos;//Var to hold player position
+	
 	void Start () {
-		Player = GameObject.Find("Player").GetComponent<PlayerMovement>();
+		Player = GameObject.Find("Player").GetComponent<PlayerMovement>();// assigns player object to player var
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Time.timeScale !=0)
+		if(Time.timeScale !=0)// if time is moving this code will execute
 		{
-			Vector3 mouse_pos = Input.mousePosition;
+			 mouse_pos = Input.mousePosition;// assigns current mouse position to var
 	
-			Vector3 player_pos = Camera.main.WorldToScreenPoint(this.transform.position);
+			 player_pos = Camera.main.WorldToScreenPoint(this.transform.position);// assigns current player position to var
 			
 			mouse_pos.x = mouse_pos.x - player_pos.x;
 			mouse_pos.y = mouse_pos.y - player_pos.y;
 			
-			float angle = Mathf.Atan2 (mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+			float angle = Mathf.Atan2 (mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;// ? it works out the angle somehow
 
 
 	
-			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);//rotates object on the angle specified in angle var
 
 
 		}
--- GhostGameb/Assets/Resources/Scripts/Flip.cs
+++ GhostGameb/Assets/Resources/Scripts/Flip.cs
@@ -1,42 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+//////////////////////////////
+// Script to flip the player//
+/////////////////////////////
 public class Flip : MonoBehaviour {
-	public 	bool facingRight = true;
-	private Vector3 MousePosition;
-	GameObject shoulder;
+	public 	bool facingRight = true;//bool to findout which way the player is currently facing
+	private Vector3 MousePosition;// var to store mouse position
+	GameObject shoulder;// var to store position of shoulder
 	// Use this for initialization
 	void Start () {
-		shoulder = GameObject.Find("ShoulderJoint");
+		shoulder = GameObject.Find("ShoulderJoint");// assigns shoulderJoint gameobject to var
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Time.timeScale !=0)
+		if(Time.timeScale !=0)// if time is moving this code executes
 		{
-		MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		transform.position = shoulder.transform.position;
+		MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);// assigns current mouse position to var
+		transform.position = shoulder.transform.position;//sets the shoulder position to the right side
 		if( MousePosition.x > transform.position.x
-		   && !facingRight)
+		   && !facingRight)// if the mouse is on the right of the player and the player is facing left this code executes
 		{
-			FlipM();
+			FlipM();//method that flips the player
 			
 		}
 		
-		else if(MousePosition.x < transform.position.x && facingRight)
+		else if(MousePosition.x < transform.position.x && facingRight)// if the mouse is on the left of the player and the player is facing right..
 		{
-			FlipM();
+			FlipM();//flips the player
 			
 		}
 		}
 	}
-	void FlipM()
+	void FlipM()// this method flips the player
 	{
-		facingRight= !facingRight;
-		Vector3 theScale = transform.localScale;
-		theScale.y *= -1;
-		theScale.x *= 1;
-		transform.localScale = theScale;
+		facingRight= !facingRight;// sets facing right to the opposite of facing rights current value(i.e true becomes fales and vice versa)
+		Vector3 theScale = transform.localScale;// var so scale can be manipulated
+		theScale.y *= -1;//flips on the y axis- need to check once sprites are in place( think its needed for 
+		// weapon flipping but not for person flipping
+		theScale.x *= 1;//flips on the x axis
+		transform.localScale = theScale;// sets the new values
 		
 		
 	}
